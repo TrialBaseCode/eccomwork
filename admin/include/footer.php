90afe5853daaddd57fc7dc31000395b6c25870f3
TrialBaseCode <compwork321@gmail.com>
2023-05-11 14:29:36 +0530

    admin commit

---
+++ admin/include/footer.php
@@ -0,0 +1,30 @@
+<footer class="footer pt-5">
+      <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-12">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+              <li class="nav-item">
+                <a href="/Aboutus" class="nav-link text-muted" target="_blank">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="/Service" class="nav-link text-muted" target="_blank">Service</a>
+              </li>
+              <li class="nav-item">
+                <a href="/blog" class="nav-link text-muted" target="_blank">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a href="/Contact" class="nav-link pe-0 text-muted" target="_blank">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
+  
+</main>
+<script src="../assets/js/core/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+
+</body>
+</html>
